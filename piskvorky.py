ce17fedaf80f719f69459a3c614ff1ba7286a7fd
PavlaH23 <46927355+PavlaH23@users.noreply.github.com>
2019-05-01 20:45:14 +0200

    Update piskvorky.py

--- piskvorky.py
+++ piskvorky.py
@@ -1,3 +1,5 @@
+import ai
+
 def tah(pole, cislo_policka, symbol):
     "Vrátí herní pole s daným symbolem umístěným na danou pozici"
     return pole[:cislo_policka] + symbol + pole[cislo_policka + 1:]
