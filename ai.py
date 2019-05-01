1f2b563d7ca0611f89f41797e80cc2abb97e610e
Pavla Houstekova <phoustekova@gmail.com>
2019-05-01 20:15:04 +0200

    Create ai.py

---
+++ ai.py
@@ -0,0 +1,11 @@
+from random import randrange
+
+def tah_pocitace(pole):
+
+    symbol = "o"
+    opakovani = 1
+    while True:
+        cislo_policka = randrange(20)
+        if pole[cislo_policka] == "-":
+            return pole[:cislo_policka] + symbol + pole[cislo_policka + 1:]
+        opakovani +=1
