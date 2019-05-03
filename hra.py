8c895aae7ffc10b4439d2f3fc5fbb3fefab95292
PavlaH23 <46927355+PavlaH23@users.noreply.github.com>
2019-05-03 10:34:59 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -1,2 +1,29 @@
 from piskvorky import tah, tah_hrace, vyhodnot
 from ai import tah_pocitace
+from util import tah
+
+pole = "-" * 20
+na_tahu = "x"
+
+while True:
+    if na_tahu == "x":
+        pole = tah_hrace(pole)
+        na_tahu = "o"
+    elif na_tahu == "o":
+        pole = tah_pocitace(pole)
+        na_tahu = "x"
+
+    vysledek = vyhodnot(pole)
+    print(pole)
+
+    if vysledek != "-":
+        if vysledek == "!":
+            print("Remiza! {}".format(pole))
+        elif vysledek == "x":
+            print("Vyhravas nad pocitacem! {}".format(pole))
+        elif vysledek == "o":
+            print("Bohuzel, pocitac vyhral. {}".format(pole))
+        else:
+            raise ValueError("Nepripustny vysledek hry '{}'".format(vysledek))
+        break
+
